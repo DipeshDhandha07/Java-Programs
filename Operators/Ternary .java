6cd0e11fd81bf66f00dc5cacc7076a5847753072
Dipesh Dhandha <55910147+DipeshDhandha07@users.noreply.github.com>
2021-08-08 16:36:24 +0530

    Add files via upload

---
+++ Operators/Ternary .java
@@ -0,0 +1,10 @@
+// ?: -is nothing but if-else statement ? for if and : for else
+class Ternary 
+{
+    public static void main(String[] args)
+    {
+        int i= 10, j=20;
+        System.out.println((i==j)?true:false);
+        System.out.println((i<j)?true:false); 
+    }
+}
